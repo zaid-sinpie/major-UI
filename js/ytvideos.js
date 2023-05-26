de6a077791f2f66fb8b175651895a2a2edda8b74
RJD02 <dulangeraviraj@gmail.com>
2023-05-26 21:52:40 +0530

    Fix the ytvideos bug

---
+++ js/ytvideos.js
@@ -0,0 +1,30 @@
+const submitBtn = document.querySelector("form button");
+const inputs = document.querySelectorAll("form input[value='yes']:checked");
+
+console.log(submitBtn);
+console.log(inputs);
+
+for (let i = 0; i < inputs.length; i++) {
+  if (inputs[i].value === "yes") console.log(inputs[i].checked);
+}
+
+const getNumberofYesCheckedInputs = () => {
+  let num = 0;
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].value === "yes") {
+      num++;
+      console.log(inputs[i].checked);
+    }
+  }
+  return num;
+};
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const numberOfYes = getNumberofYesCheckedInputs();
+  if (numberOfYes > 0) {
+    window.location.assign("/pages/parterquizes.html");
+  } else {
+    window.location.assign("/pages/congrats.html");
+  }
+});
