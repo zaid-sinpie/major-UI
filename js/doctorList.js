316ddfd5f8baa0c7440717c8d0b3b6668a627b29
RJD02 <dulangeraviraj@gmail.com>
2023-05-29 22:57:40 +0530

    Add logic for doctor list page

--- js/doctorList.js
+++ js/doctorList.js
@@ -1,33 +1,73 @@
-const listDiv = document.querySelector(".doctor-list");
+// const listDiv = document.querySelector(".doctor-list");
 
-const getAllRecommendedDoctors = async () => {
-  const doctorList = [];
-  let i = 0;
-  while (`doctor${i}` in localStorage) {
-    const doctorId = localStorage.getItem(`doctor${i}`);
-    const response = await fetch(
-      `http://localhost:8000/api/doctor/${doctorId}`
-    );
-    const data = await response.json();
-    const doctor = await data.doctor;
-    doctorList.push(doctor);
-    i++;
-  }
-  return doctorList;
+// const getAllRecommendedDoctors = async () => {
+//   const doctorList = [];
+//   let i = 0;
+//   while (`doctor${i}` in localStorage) {
+//     const doctorId = localStorage.getItem(`doctor${i}`);
+//     const response = await fetch(
+//       `http://localhost:8000/api/doctor/${doctorId}`
+//     );
+//     const data = await response.json();
+//     const doctor = await data.doctor;
+//     doctorList.push(doctor);
+//     i++;
+//   }
+//   return doctorList;
+// };
+
+// const deleteAllDoctorsFromLocalStorage = () => {
+//   let i = 0;
+//   while (`doctor${i}` in localStorage) {
+//     localStorage.removeItem(`doctor${i}`);
+//   }
+// };
+
+// const ListOutAllDoctors = async () => {
+//   const doctorList = await getAllRecommendedDoctors();
+//   console.log(doctorList);
+//   for (let index = 0; index < doctorList.length; index++) {
+//     const element = doctorList[index];
+//     const doctorDiv = document.createElement("div");
+//     const namePara = document.createElement("p");
+//     const nameText = document.createTextNode(`Name: ${element.name}`);
+//     namePara.appendChild(nameText);
+//     doctorDiv.appendChild(namePara);
+//     const emailPara = document.createElement("p");
+//     const emailText = document.createTextNode(`Email: ${element.email}`);
+//     emailPara.appendChild(emailText);
+//     doctorDiv.appendChild(emailPara);
+//     const tagsPara = document.createElement("p");
+//     let tagText = "";
+//     for (let i = 0; i < element.tags.length; i++) {
+//       tagText += element.tags[i];
+//       if (i != element.tags.length - 1) {
+//         tagText += ",";
+//       }
+//     }
+//     tagText = document.createTextNode("Specializes in: " + tagText);
+//     tagsPara.appendChild(tagText);
+//     doctorDiv.appendChild(tagsPara);
+//     listDiv.appendChild(doctorDiv);
+//   }
+
+//   deleteAllDoctorsFromLocalStorage();
+// };
+
+// ListOutAllDoctors();
+
+const doctorListDiv = document.querySelector(".doctor-list");
+
+const getAllDoctors = async () => {
+  const response = await fetch("http://localhost:8000/api/doctors");
+  const data = await response.json();
+  return data.doctors;
 };
 
-const deleteAllDoctorsFromLocalStorage = () => {
-  let i = 0;
-  while (`doctor${i}` in localStorage) {
-    localStorage.removeItem(`doctor${i}`);
-  }
-};
-
-const ListOutAllDoctors = async () => {
-  const doctorList = await getAllRecommendedDoctors();
-  console.log(doctorList);
-  for (let index = 0; index < doctorList.length; index++) {
-    const element = doctorList[index];
+const provideList = async () => {
+  const doctorList = await getAllDoctors();
+  for (let i = 0; i < doctorList.length; i++) {
+    const element = doctorList[i];
     const doctorDiv = document.createElement("div");
     const namePara = document.createElement("p");
     const nameText = document.createTextNode(`Name: ${element.name}`);
@@ -39,19 +79,18 @@ const ListOutAllDoctors = async () => {
     doctorDiv.appendChild(emailPara);
     const tagsPara = document.createElement("p");
     let tagText = "";
-    for (let i = 0; i < element.tags.length; i++) {
-      tagText += element.tags[i];
-      if (i != element.tags.length - 1) {
+    for (let j = 0; j < element.tags.length; j++) {
+      tagText += element.tags[j];
+      if (j != element.tags.length - 1) {
         tagText += ",";
       }
     }
-    tagText = document.createTextNode("Specializes in: " + tagText);
+    tagText = document.createTextNode(`Specializes in: ${tagText}`);
     tagsPara.appendChild(tagText);
     doctorDiv.appendChild(tagsPara);
-    listDiv.appendChild(doctorDiv);
+    doctorListDiv.appendChild(doctorDiv);
+    doctorDiv.classList.add("doctor");
   }
-
-  deleteAllDoctorsFromLocalStorage();
 };
 
-ListOutAllDoctors();
+provideList();
