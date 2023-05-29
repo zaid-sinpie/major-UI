8522c44a75c0c4a8931cc404dea5c2010eb0fe05
RJD02 <dulangeraviraj@gmail.com>
2023-05-30 01:58:50 +0530

    Add doctor list rendering logic

---
+++ js/index_parent.js
@@ -0,0 +1,33 @@
+const listBtn = document.querySelector(".btn");
+
+const getChildrenDetails = async () => {
+  // const id = localStorage.getItem('userId')
+  const response = await fetch(
+    "http://localhost:8000/api/parent/detail?token=" +
+      localStorage.getItem("token"),
+    {
+      method: "POST",
+      body: JSON.stringify({
+        token: localStorage.getItem("token"),
+        email: localStorage.getItem("email"),
+        userId: localStorage.getItem("userId"),
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const data = await response.json();
+  console.log(data);
+  return data.parent.children[0];
+};
+const provideDoctorList = async () => {
+  const child = await getChildrenDetails();
+  if (child.isHealthy) {
+    alert("Your child is healthy!");
+  } else {
+    window.location.assign("/pages/doctorList.html");
+  }
+};
+
+listBtn.addEventListener("click", provideDoctorList);
